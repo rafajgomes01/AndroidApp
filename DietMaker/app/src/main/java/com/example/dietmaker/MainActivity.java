deeffd031f41c176aeff2e0438b579bbf3c737fe
Igor <igor.lolatto@uni9.edu.br>
2019-10-04 00:51:45 -0300

    iconesbonitos

--- DietMaker/app/src/main/java/com/example/dietmaker/MainActivity.java
+++ DietMaker/app/src/main/java/com/example/dietmaker/MainActivity.java
@@ -70,11 +70,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
+
         return super.onOptionsItemSelected(item);
     }
 
