a7c0bae315e99cd00f76ede3c99f52fb6c67f7ef
Ítalo Leandro <italo-leandro1@hotmail.com>
2019-10-09 11:17:17 -0300

    TelaNovaTeladeCarregamentoByIgor
    foi adicionado um fragment novo para teste, foi atualizado o icone e também fiz uma tela de carregamento de incicio

--- DietMaker/app/src/main/java/com/example/dietmaker/MainActivity.java
+++ DietMaker/app/src/main/java/com/example/dietmaker/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.dietmaker;
 
-import android.content.ClipData;
+import android.net.Uri;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
@@ -23,16 +19,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TelaNovaTeste.OnFragmentInteractionListener{
 
     private AppBarConfiguration mAppBarConfiguration;
-    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,28 +37,15 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.testeFragment)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        listView=(ListView)findViewById(R.id.listview);
-
-        ArrayList<String> arrayList=new ArrayList<>();
 
 
-        arrayList.add("oi");
-        arrayList.add("oi");
-        arrayList.add("oi");
-        arrayList.add("oi");
-        arrayList.add("oi");
-        arrayList.add("oi");
-        arrayList.add("oi");
-        arrayList.add("oi");
 
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,arrayList);
-        listView.setAdapter((arrayAdapter));
     }
 
     @Override
@@ -107,4 +85,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
--- DietMaker/app/src/main/java/com/example/dietmaker/ui/gallery/GalleryFragment.java
+++ DietMaker/app/src/main/java/com/example/dietmaker/ui/gallery/GalleryFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,8 +16,10 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.dietmaker.R;
 
-public class GalleryFragment extends Fragment {
+import java.util.ArrayList;
 
+public class GalleryFragment extends Fragment {
+    ListView listView;
     private GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -29,6 +33,25 @@ public class GalleryFragment extends Fragment {
 
             }
         });
+
+        listView=(ListView)root.findViewById(R.id.listview);
+        ArrayList<String> arrayList=new ArrayList<>();
+
+
+        arrayList.add("oi");
+        arrayList.add("oi");
+        arrayList.add("oi");
+        arrayList.add("oi");
+        arrayList.add("oi");
+        arrayList.add("oi");
+        arrayList.add("oi");
+        arrayList.add("oi");
+
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(getContext(), android.R.layout.simple_expandable_list_item_1,arrayList);
+
+        listView.setAdapter((arrayAdapter));
+
         return root;
     }
 }
