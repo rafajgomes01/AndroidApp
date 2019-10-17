c26ca04e1fcc5c135fb009e0294944c6dd252f68
Ítalo Leandro <italo-leandro1@hotmail.com>
2019-10-17 01:59:16 -0300

    Add menu

--- DietMaker/app/src/main/java/com/example/dietmaker/PerderPeso.java
+++ DietMaker/app/src/main/java/com/example/dietmaker/PerderPeso.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
  * create an instance of this fragment.
  */
 public class PerderPeso extends Fragment {
-    
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
